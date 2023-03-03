026fcc2be55e8ffe45960d9e059790f508d82694
Dang Thi My Thanh <ti16102000@gmail.com>
2023-03-03 20:23:31 +0700

    add logo left in header and add animation for hero carousel in home page

--- js/script.js
+++ js/script.js
@@ -24,21 +24,21 @@ function logoutUser(){
 
 //slide hero carousel
 let slideIndex = 0;
-showSlides();
+// showSlides();
 
-function showSlides() {
-    let i;
-    let slides = document.getElementsByClassName("carousel__item");
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-    }
-    slideIndex++;
-    if (slideIndex > slides.length) {
-        slideIndex = 1
-    }
-    slides[slideIndex - 1].style.display = "block";
-    setTimeout(showSlides, 3000); // Change image every 3 seconds
-}
+// function showSlides() {
+//     let i;
+//     let slides = document.getElementsByClassName("carousel__item");
+//     for (i = 0; i < slides.length; i++) {
+//         slides[i].style.display = "none";
+//     }
+//     slideIndex++;
+//     if (slideIndex > slides.length) {
+//         slideIndex = 1
+//     }
+//     slides[slideIndex - 1].style.display = "block";
+//     setTimeout(showSlides, 3000); // Change image every 3 seconds
+// }
 //articles tab
 function openTab(idTab) {
     var i;
